5181ee69a5f1eb2fc6d2c063a900cf009b102bb6
NJ <nj.huang@gamer.com.tw>
2024-03-11 12:08:18 +0800

    新增主視覺進場、專欄調整

--- js/animate.js
+++ js/animate.js
@@ -18,4 +18,54 @@ $(function () {
     }
 scrollAnim();
 $(document).on("scroll", scrollAnim);
+});
+
+
+anime({
+    targets: '.section-kv__main-title',
+    scale: [0, 1],
+    easing: 'easeOutElastic(1.5, 1)',
+    duration: 2000,
+    delay: anime.stagger(100),
+})
+
+anime({
+    targets: '.section-kv__sec-title',
+    translateY: [150, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutBack',
+    duration: 2000,
+    delay: anime.stagger(100),
+    rotate: function() {
+    return anime(-15, 15);
+    },
+})
+
+anime({
+    targets: '.section-kv__circle',
+    scale: [0, 1],
+    opacity: [0, 1],
+    easing: 'easeOutElastic(1.5, 1)',
+    duration: 1500,
+    delay: anime.stagger(100),
+})
+
+anime({
+    targets: '.icon-line',
+    scale: [0, 1],
+    opacity: [0, 1],
+    easing: 'easeOutElastic(2.5, 1)',
+    duration: 1500,
+    delay: anime.stagger(500),
+})
+
+
+anime({
+    targets: '.section-kv__d-girl',
+    translateY: [1000, 1],
+    scale: [0, 1],
+    opacity: [0, 1],
+    easing: 'easeOutElastic(2.5, 1)',
+    duration: 1500,
+    delay: anime.stagger(100),
 })
